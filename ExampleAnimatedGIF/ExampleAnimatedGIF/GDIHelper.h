63f22e143e541899b7678df66da197cb2032be30
Poral <9017938+SeynalKim@users.noreply.github.com>
2021-03-13 10:25:12 +0800

    Update GDIHelper.h

--- ExampleAnimatedGIF/ExampleAnimatedGIF/GDIHelper.h
+++ ExampleAnimatedGIF/ExampleAnimatedGIF/GDIHelper.h
@@ -8,26 +8,27 @@ class GDIHelper {
 
 public:
 	GDIHelper();
-	void LoadImageFromResource(HWND hWnd, UINT_PTR timer_id, HMODULE hMod, const wchar_t* resid, const wchar_t* restype);
-	void LoadImageFromFile(HWND hWnd, UINT_PTR timer_id, string file_location, const wchar_t* restype);
-	void OnTimer();
-	void Desrtroy();
-	void Stop(UINT_PTR timer_id);
-	void DrawItem(HDC hdc, int xPosition, int yPosition, int width, int height);
+	void DisplayImageFromResource(HMODULE hMod, const wchar_t* resid, const wchar_t* restype, HWND hWnd, UINT_PTR unique_id, int xPosition, int yPosition, int width, int hheight);
+	void DisplayImageFromFile(string file_location, HWND hWnd, UINT_PTR unique_id, int xPosition, int yPosition, int width, int height);
+	void Destroy();
 	
 private:
-	void InitializeImage();
+	static LRESULT CALLBACK StaticControlProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData);
+	void GetImageFrame();
+	void SetImage(int uunique_id, int xxPosition, int yyPosition, int wwidth, int hheight, Image* image, HWND hwnd);
 	bool IsFileExist(string file_name);
-	void AnimateGIF();
-	HWND hwnd;
-	Image* m_pImage;
-	GUID* m_pDimensionIDs;
-	UINT m_FrameCount;
-	PropertyItem* m_pItem;
-	UINT m_iCurrentFrame;
-	UINT_PTR timer_id;
-	BOOL m_bIsPlaying;
-	BOOL isPlayable;
+	static void run();
+
+	static HWND staticControl;
+	static Image* m_pImage;
+	static GUID* m_pDimensionIDs;
+	static UINT m_FrameCount;
+	static PropertyItem* m_pItem;
+	static UINT m_iCurrentFrame;
+	static UINT_PTR unique_id;
+	static BOOL m_bIsPlaying;
+	static BOOL isPlayable;
+	static int xPosition, yPosition, width, height, animation_duration;
 };
 
 #endif
