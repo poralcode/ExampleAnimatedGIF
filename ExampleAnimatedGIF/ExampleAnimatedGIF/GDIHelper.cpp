7e8e15931ec56a3a12f21a7d0e804716a3b4a1e7
Poral <9017938+SeynalKim@users.noreply.github.com>
2021-03-13 10:19:07 +0800

    Update GDIHelper.cpp

--- ExampleAnimatedGIF/ExampleAnimatedGIF/GDIHelper.cpp
+++ ExampleAnimatedGIF/ExampleAnimatedGIF/GDIHelper.cpp
@@ -1,20 +1,25 @@
 #include "GDIHelper.h"
 
+HWND GDIHelper::staticControl = NULL;
+Image* GDIHelper::m_pImage = NULL;
+GUID* GDIHelper::m_pDimensionIDs = NULL;
+UINT GDIHelper::m_FrameCount = 0;
+PropertyItem* GDIHelper::m_pItem = NULL;
+UINT GDIHelper::m_iCurrentFrame = 0;
+UINT_PTR GDIHelper::unique_id = 0;
+BOOL GDIHelper::m_bIsPlaying = FALSE;
+BOOL GDIHelper::isPlayable = FALSE;
+int GDIHelper::xPosition = 0;
+int GDIHelper::yPosition = 0;
+int GDIHelper::width = 0;
+int GDIHelper::height = 0;
+int GDIHelper::animation_duration = 0;
+
 /** GDIHelper is a class helper to display images and animated GIF **/
-GDIHelper::GDIHelper() {
-    timer_id = 0;
-    m_FrameCount = 0;
-    m_iCurrentFrame = 0;
-    m_pImage = NULL;
-    m_pDimensionIDs = NULL;
-    m_pItem = NULL;
-    hwnd = NULL;
-    m_bIsPlaying = FALSE;
-    isPlayable = FALSE;
-}
+GDIHelper::GDIHelper() {}
 
 /** Function to destroy objects and arrays, call this function on WM_DESTROY of WinProc. **/
-void GDIHelper::Desrtroy() {
+void GDIHelper::Destroy() {
     if(m_pDimensionIDs) {
         delete[] m_pDimensionIDs;
     }
@@ -26,54 +31,31 @@ void GDIHelper::Desrtroy() {
     if(m_pImage) {
         delete m_pImage;
     }
-}
-
-/** Function to stop the animation of GIF. **/
-void GDIHelper::Stop(UINT_PTR ttimer_id) {
-    if(m_bIsPlaying == FALSE) {
-        return;
-    }
-
-    KillTimer(hwnd, ttimer_id);
     m_bIsPlaying = FALSE;
     isPlayable = FALSE;
+    RemoveWindowSubclass(staticControl, &StaticControlProc, unique_id);
 }
 
-
-/** Functon to load the next frame of GIF, must be call on WM_TIMER. **/
-void GDIHelper::OnTimer() {
-    if(isPlayable) {
-        KillTimer(hwnd, timer_id);
-        GUID Guid = FrameDimensionTime;
-        m_pImage->SelectActiveFrame(&Guid, m_iCurrentFrame);
-        SetTimer(hwnd, 120, ((UINT*)m_pItem[0].value)[m_iCurrentFrame] * 10, NULL);
-        m_iCurrentFrame = (++m_iCurrentFrame) % m_FrameCount;
-        InvalidateRect(hwnd, NULL, FALSE);
-    }
-}
-
-/** Private function, call this to animate the GIF image, should be call before drawing the image usually on WM_PAINT. **/
-void GDIHelper::AnimateGIF() {
+/** Private function, call this function as thread to animate the GIF image. **/
+void GDIHelper::run() {
     if(m_bIsPlaying == TRUE) {
         return;
     }
-    if(isPlayable) {
-        m_iCurrentFrame = 0;
-        GUID Guid = FrameDimensionTime;
+    m_iCurrentFrame = 0;
+    GUID Guid = FrameDimensionTime;
+    m_pImage->SelectActiveFrame(&Guid, m_iCurrentFrame);
+    ++m_iCurrentFrame;
+    m_bIsPlaying = TRUE;
+    animation_duration = ((UINT*)m_pItem[0].value)[m_iCurrentFrame] * 10;
+    
+    while(isPlayable) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(animation_duration));
         m_pImage->SelectActiveFrame(&Guid, m_iCurrentFrame);
-        SetTimer(hwnd, 120, ((UINT*)m_pItem[0].value)[m_iCurrentFrame] * 10, NULL);
-        ++m_iCurrentFrame;
-        InvalidateRect(hwnd, NULL, FALSE);
-        m_bIsPlaying = TRUE;
+        m_iCurrentFrame = (++m_iCurrentFrame) % m_FrameCount;
+        InvalidateRect(staticControl, NULL, FALSE);
+        UpdateWindow(staticControl);
     }
 }
-/** Function to draw the image in Window, must be call on WM_PAINT. **/
-void GDIHelper::DrawItem(HDC hdc, int xPosition, int yPosition, int width, int height) {
-    AnimateGIF(); //This will only works if the image has more than one frame.
-    Graphics g(hdc);
-    g.DrawImage(m_pImage, xPosition, yPosition, width, height);
-}
-
 
 /** Private function, accessible only in this class, check if file exist. **/
 bool GDIHelper::IsFileExist(string file_name) {
@@ -82,43 +64,69 @@ bool GDIHelper::IsFileExist(string file_name) {
 }
 
 /** Private function, function to count and get the frame of image. **/
-void GDIHelper::InitializeImage() {
+void GDIHelper::GetImageFrame() {
     UINT count = m_pImage->GetFrameDimensionsCount();
     m_pDimensionIDs = new GUID[count];
     m_pImage->GetFrameDimensionsList(m_pDimensionIDs, count);
 
-    WCHAR strGuid[39];
-    (void)StringFromGUID2(m_pDimensionIDs[0], strGuid, 39);
     m_FrameCount = m_pImage->GetFrameCount(&m_pDimensionIDs[0]);
-
-    if(m_FrameCount > 1) { //frame of GIF is more than one, all good, we don't want the error of `Access violation reading location`
-        isPlayable = TRUE;
-        OutputDebugString(_T("ERROR: GDIHelper::InitializeImage >> Image file contains more than 1 frame, its playable.\n"));
-    }
 
     UINT TotalBuffer = m_pImage->GetPropertyItemSize(PropertyTagFrameDelay);
     m_pItem = (PropertyItem*)malloc(TotalBuffer);
     m_pImage->GetPropertyItem(PropertyTagFrameDelay, TotalBuffer, m_pItem);
+
+    if(m_FrameCount > 1) {  // frame of GIF is more than one, all good, we don't want the error of `Access violation reading location`
+        OutputDebugString(_T("NOTICED: GDIHelper::InitializeImage >> Image file has more than 1 frame, its playable (2).\n"));
+        isPlayable = TRUE;  // is playable
+        std::thread t(run); // Start the animation as thread.
+        t.detach();         // this will be non-blocking thread.
+    }
 }
 
-/** Function to Load Image from Local File. **/
-void GDIHelper::LoadImageFromFile(HWND hWnd, UINT_PTR ttimer_id, string file_name, const wchar_t* restype) {
-    hwnd = hWnd;
-    timer_id = ttimer_id;
-    
+LRESULT CALLBACK GDIHelper::StaticControlProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR uIdSubclass, DWORD_PTR dwRefData) {
+    switch(uMsg) {
+        case WM_PAINT:{
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            Graphics g(hdc);
+            g.DrawImage(m_pImage, 0, 0, width, height);
+
+            EndPaint(hwnd, &ps);
+            return TRUE; 
+        }
+    }
+   return DefSubclassProc(hwnd, uMsg, wParam, lParam);
+}
+
+/** Function to set the image and initialize all required variables. **/
+void GDIHelper::SetImage(int uunique_id, int xxPosition, int yyPosition, int wwidth, int hheight, Image* image, HWND hwnd) {
+    unique_id = uunique_id;
+    xPosition = xxPosition;
+    yPosition = yyPosition;
+    width = wwidth;
+    height = hheight;
+
+    staticControl = CreateWindowEx(0, L"STATIC", NULL, WS_CHILD | WS_VISIBLE | SS_OWNERDRAW, xPosition, yPosition, width, height, hwnd, NULL, NULL, NULL); //create the static control.
+    SetWindowSubclass(staticControl, &StaticControlProc, unique_id, 0);
+
+    m_pImage = image;
+    GetImageFrame(); //Initialize the image.
+}
+
+/** Function to Display Image from Local File. **/
+void GDIHelper::DisplayImageFromFile(string file_name, HWND hWnd, UINT_PTR uunique_id, int xxPosition, int yyPosition, int wwidth, int hheight) {
     if(!IsFileExist(file_name)) {
         OutputDebugString(_T("ERROR: GDIHelper::LoadImageFromFile >> Invalid file or not exist\n"));
         return; 
     }
+
     std::wstring widestr = std::wstring(file_name.begin(), file_name.end()); // Convert the string file_name to wstring.
-    m_pImage = Image::FromFile(widestr.c_str()); //Convert the wtring to wchar and initialize.
-    InitializeImage(); //Initialize the image.
+    SetImage(uunique_id, xxPosition, yyPosition, wwidth, hheight, Image::FromFile(widestr.c_str()), hWnd); //Set image and Control
 }
 
 /** Function to Load Image from Resources. **/
-void GDIHelper::LoadImageFromResource(HWND hWnd, UINT_PTR ttimer_id, HMODULE hMod, const wchar_t* resid, const wchar_t* restype) {
-    hwnd = hWnd;
-    timer_id = ttimer_id;
+void GDIHelper::DisplayImageFromResource(HMODULE hMod, const wchar_t* resid, const wchar_t* restype, HWND hWnd, UINT_PTR uunique_id, int xxPosition, int yyPosition, int wwidth, int hheight) {
 
     IStream* pStream = nullptr;
     Gdiplus::Bitmap* pBmp = nullptr;
@@ -132,16 +140,14 @@ void GDIHelper::LoadImageFromResource(HWND hWnd, UINT_PTR ttimer_id, HMODULE hMo
             if(hGlobalResource) {
                 void* imagebytes = LockResource(hGlobalResource); // get a pointer to the file bytes
 
-                // copy image bytes into a real hglobal memory handle
-                hGlobal = ::GlobalAlloc(GHND, dwResourceSize);
+                hGlobal = ::GlobalAlloc(GHND, dwResourceSize); // copy image bytes into a real hglobal memory handle
                 if(hGlobal) {
                     void* pBuffer = ::GlobalLock(hGlobal);
                     if(pBuffer) {
                         memcpy(pBuffer, imagebytes, dwResourceSize);
                         HRESULT hr = CreateStreamOnHGlobal(hGlobal, TRUE, &pStream);
                         if(SUCCEEDED(hr)) {
-                            // pStream now owns the global handle and will invoke GlobalFree on release
-                            hGlobal = nullptr;
+                            hGlobal = nullptr; // pStream now owns the global handle and will invoke GlobalFree on release
                             pBmp = new Gdiplus::Bitmap(pStream);
                         }
                     }
@@ -159,6 +165,5 @@ void GDIHelper::LoadImageFromResource(HWND hWnd, UINT_PTR ttimer_id, HMODULE hMo
         GlobalFree(hGlobal);
     }
 
-    m_pImage = pBmp; //Set image from resources.
-    InitializeImage(); //Initialize the image.
+    SetImage(uunique_id, xxPosition, yyPosition, wwidth, hheight, pBmp, hWnd);
 }
