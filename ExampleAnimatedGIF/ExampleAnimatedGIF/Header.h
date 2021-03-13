e0418fd28b549119e70a4daf54ff2c262df3a254
Poral <9017938+SeynalKim@users.noreply.github.com>
2021-03-13 10:25:43 +0800

    Update Header.h

--- ExampleAnimatedGIF/ExampleAnimatedGIF/Header.h
+++ ExampleAnimatedGIF/ExampleAnimatedGIF/Header.h
@@ -1,6 +1,8 @@
-#include <stdexcept>					//For throwing exceptions
 #include <windows.h>					//GDI+ include in order (1) 
+#include <commctrl.h>
 #include <tchar.h>
+#include <thread>
+#include <chrono>
 #include <string>
 #include <objidl.h>						//GDI+ include in order (2)
 #include <gdiplus.h>					//GDI+ include in order (3)
@@ -8,5 +10,6 @@ using namespace std;
 using namespace Gdiplus;				//GDI+ include in order (4)
 #pragma comment (lib,"Gdiplus.lib")		//GDI+ include in order (5)
 #pragma comment(lib, "dwmapi.lib")		//GDI+ include in order (6)
+#pragma comment(lib, "comctl32.lib")
 
 /** Don't forget to initialize the GDI+ **/
